e849a504fdb1f0376ef37629d79199c69efeb664
John-Oliv <johnatha.oliveira.pessoal@gmail.com>
2021-10-15 20:32:58 -0300

    Draw the game grid

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World");
+    printf("     |     |     ");
+    printf("\n_____|_____|_____");
+    printf("\n     |     |     ");
+    printf("\n_____|_____|_____");
+    printf("\n     |     |     ");
+    printf("\n     |     |     ");
 }
