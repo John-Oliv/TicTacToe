c48b7717c1433d37901116c5749b9b78a54963d0
John-Oliv <johnatha.oliveira.pessoal@gmail.com>
2021-10-18 12:12:42 -0300

    Render the game grid

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int game_grid(int position_value[]) {
+int gameGrid(int positionValue[]) {
     printf("\n     |     |     ");
-    printf("\n__%d__|__%d__|__%d__", position_value[0], position_value[1], position_value[2]);
+    printf("\n__%d__|__%d__|__%d__", positionValue[0], positionValue[1], positionValue[2]);
     printf("\n     |     |     ");
-    printf("\n__%d__|__%d__|__%d__", position_value[3], position_value[4], position_value[5]);
+    printf("\n__%d__|__%d__|__%d__", positionValue[3], positionValue[4], positionValue[5]);
     printf("\n     |     |     ");
-    printf("\n  %d  |  %d  |  %d  ", position_value[6], position_value[7], position_value[8]);
+    printf("\n  %d  |  %d  |  %d  ", positionValue[6], positionValue[7], positionValue[8]);
+    return 0;
 }
 
 int main() {
-    int position_value[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    game_grid(position_value);
+    int positionValue[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    gameGrid(positionValue);
 }
