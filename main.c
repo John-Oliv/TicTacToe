cac1c290993b392308eaaa21b41dd4bfb192eb0e
John-Oliv <john.oliveira.pessoal@gmail.com>
2021-11-08 07:15:19 -0300

    Improvements in the code modularity

--- main.c
+++ main.c
@@ -9,20 +9,28 @@ void gameGrid(int positionValue[]) {
     printf("\n  %c  |  %c  |  %c  ", positionValue[6], positionValue[7], positionValue[8]);
 }
 
-int main() {
-    int positionValue[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    int playerOneInput, playerTwoInput;
-    gameGrid(positionValue);
+void assignPlayerOnePosition(int positionValue[], int playerOneInput) {
     printf("\nPlayer 1 please input the position you would like to put the X: ");
     scanf("%d", &playerOneInput);
     if (playerOneInput + 48 == positionValue[playerOneInput]) {
         positionValue[playerOneInput] = ('X');
         gameGrid(positionValue);
     }
+}
+
+void assignPlayerTwoPosition(int positionValue[], int playerTwoInput) {
     printf("\nPlayer 2 please input the position you would like to put the O: ");
     scanf("%d", &playerTwoInput);
     if (playerTwoInput + 48 == positionValue[playerTwoInput]) {
         positionValue[playerTwoInput] = ('O');
-        gameGrid(positionValue);
+            gameGrid(positionValue);
     }
+}
+
+int main() {
+    int positionValue[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int playerOneInput, playerTwoInput;
+    gameGrid(positionValue);
+    assignPlayerOnePosition(positionValue, playerOneInput);
+    assignPlayerTwoPosition(positionValue, playerTwoInput);
 }
