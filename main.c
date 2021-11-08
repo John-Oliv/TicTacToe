8d033773f8d8f1a8380e152e974c6e75603ec0ba
John-Oliv <john.oliveira.pessoal@gmail.com>
2021-11-08 06:58:27 -0300

    Assign players position

--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
-int gameGrid(int positionValue[]) {
+void gameGrid(int positionValue[]) {
     printf("\n     |     |     ");
-    printf("\n__%d__|__%d__|__%d__", positionValue[0], positionValue[1], positionValue[2]);
+    printf("\n__%c__|__%c__|__%c__", positionValue[0], positionValue[1], positionValue[2]);
     printf("\n     |     |     ");
-    printf("\n__%d__|__%d__|__%d__", positionValue[3], positionValue[4], positionValue[5]);
+    printf("\n__%c__|__%c__|__%c__", positionValue[3], positionValue[4], positionValue[5]);
     printf("\n     |     |     ");
-    printf("\n  %d  |  %d  |  %d  ", positionValue[6], positionValue[7], positionValue[8]);
-    return 0;
+    printf("\n  %c  |  %c  |  %c  ", positionValue[6], positionValue[7], positionValue[8]);
 }
 
 int main() {
-    int positionValue[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int positionValue[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int playerOneInput, playerTwoInput;
     gameGrid(positionValue);
+    printf("\nPlayer 1 please input the position you would like to put the X: ");
+    scanf("%d", &playerOneInput);
+    if (playerOneInput + 48 == positionValue[playerOneInput]) {
+        positionValue[playerOneInput] = ('X');
+        gameGrid(positionValue);
+    }
+    printf("\nPlayer 2 please input the position you would like to put the O: ");
+    scanf("%d", &playerTwoInput);
+    if (playerTwoInput + 48 == positionValue[playerTwoInput]) {
+        positionValue[playerTwoInput] = ('O');
+        gameGrid(positionValue);
+    }
 }
